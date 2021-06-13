72f57d9826423a14769c8c214c8093b164badc76
Divyansh Surana <divyanshsurana2106@gmail.com>
2021-06-13 22:38:42 +0530

    Task3

---
+++ resources/views/edit.blade.php
@@ -0,0 +1,97 @@
+@extends('layout')
+
+
+
+@section('content')
+
+    <div class="row">
+
+        <div class="col-lg-12 margin-tb">
+
+            <div class="pull-left">
+
+                <h2>Edit</h2>
+
+            </div>
+
+            <div class="pull-right">
+
+                <a class="btn btn-primary" href="{{ route('products.index') }}"> Back</a>
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+    @if ($errors->any())
+
+        <div class="alert alert-danger">
+
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+
+            <ul>
+
+                @foreach ($errors->all() as $error)
+
+                    <li>{{ $error }}</li>
+
+                @endforeach
+
+            </ul>
+
+        </div>
+
+    @endif
+
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <form action="{{ route('products.update',$product->id) }}" method="POST">
+
+        @csrf
+
+        @method('PUT')
+
+
+
+         <div class="row">
+
+            <div class="col-xs-12 col-sm-12 col-md-12">
+
+                <div class="form-group">
+
+                    <strong>Title:</strong>
+
+                    <input type="text" name="name" value="{{ $product->name }}" class="form-control" placeholder="Name">
+
+                </div>
+
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-12">
+
+                <div class="form-group">
+
+                    <strong>Decribtion:</strong>
+
+                    <textarea class="form-control" style="height:150px" name="detail" placeholder="Detail">{{ $product->detail }}</textarea>
+
+                </div>
+
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+
+              <button type="submit" class="btn btn-primary">Submit</button>
+
+            </div>
+
+        </div>
+
+
+
+    </form>
+
+@endsection
